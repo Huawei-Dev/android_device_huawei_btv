7ad736bd71c71f42600b56b453491e293cd0897c
A2L5E0X1 <alexed25@protonmail.com>
2022-01-20 23:21:16 +0100

    lights: fix buildissues on Q/R
    Change-Id: I121f2342a6f8753c5d2df78023ba9a07043c3ca3

--- lights/Android.mk
+++ lights/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SHARED_LIBRARIES := liblog libhardware
 LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
