1c60446ab28c82c4d8f00f328fe4042b1f32660c
R0rt1z2 <me@r0rt1z2.com>
2023-07-13 22:53:09 +0000

    libxcollie: Use `ALOGV()` rather than `ALOGI()`
    - Otherwise we're going to get constant spam.

--- libxcollie/xcollie.c
+++ libxcollie/xcollie.c
@@ -6,15 +6,15 @@
 
 unsigned long __xcollie_start_ex(const char* name, int size, void (*arg3)() __unused,
                                  const char* arg4, unsigned int arg5) {
-    ALOGI("%s: name: %s, size: %d, arg4: %s, arg5: 0x%x", __func__, name, size,
+    ALOGV("%s: name: %s, size: %d, arg4: %s, arg5: 0x%x", __func__, name, size,
           arg4 ? arg4 : "NULL", arg5);
     return 0;
 }
 
 void xcollie_end(unsigned long handle) {
-    ALOGI("%s: process_handle: %lu", __func__, handle);
+    ALOGV("%s: process_handle: %lu", __func__, handle);
 }
 
 void xcollie_init() {
-    ALOGI("%s: called", __func__);
+    ALOGV("%s: called", __func__);
 }
