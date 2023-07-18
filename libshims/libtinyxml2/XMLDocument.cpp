d59b314158e8f83dc18f7aea456bef550c2a450d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-18 11:37:28 +0200

    libshims: Add libtinyxml2 shim
    Change-Id: I2bc7963086f7f5df453e135819ed8b28c4d6a0d5

--- libshims/libtinyxml2/XMLDocument.cpp
+++ libshims/libtinyxml2/XMLDocument.cpp
@@ -11,7 +11,12 @@ namespace tinyxml2 {
 
 extern "C" {
 void* _ZN8tinyxml211XMLDocumentC1Eb(XMLDocument* thisptr, bool processEntities) {
-    return new (thisptr) XMLDocument(processEntities, PRESERVE_WHITESPACE);
-	}
-    }
+    return new (thisptr) XMLDocument(processEntities);
+}
+
+XMLError _ZN8tinyxml211XMLDocument8SaveFileEPKc(XMLDocument* thisptr, const char* filename) {
+    return thisptr->SaveFile(filename);
+}
+}
+
 }  // namespace tinyxml2
