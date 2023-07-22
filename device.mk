89c1c020de2a42733d5f2f04852de699ab51ffce
R0rt1z2 <roger@r0rt1z2.com>
2023-07-22 21:40:58 +0000

    Switch to prebuilt camera provider
    Change-Id: I96b3c706cad29c492fb901d9b0e75964fbc46be7

--- device.mk
+++ device.mk
@@ -100,7 +100,6 @@ PRODUCT_PACKAGES += \
     
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
 # Configstore
