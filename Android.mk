5ddf33e0d68df600651cd9ceda479e0a6da81b8d
Huawei-Dev <surdu_petru@yahoo.com>
2023-10-28 10:08:43 +0200

    get rid of wifi symlink

--- Android.mk
+++ Android.mk
@@ -19,13 +19,6 @@ ifneq ($(filter btv,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-# Wifi symlink 
-WIFI_SYS := $(TARGET_OUT)/etc/wifi
-$(WIFI_SYS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/etc/wifi $@
-
 # EGL symlinks
 EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.hi3650.so
 
@@ -49,5 +42,5 @@ $(NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
 	@echo "Move vendor native_packages.bin to native_packages.xml"
 	$(hide) mv $(TARGET_OUT_VENDOR)/etc/native_packages.bin $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(WIFI_SYS) $(NATIVE_PACKAGES_FIXUP) $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += $(NATIVE_PACKAGES_FIXUP) $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
 endif
